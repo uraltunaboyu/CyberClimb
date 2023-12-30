e707d81b53e1d867c4bb99797b7a33d2b54cfde7
terreratman <53959686+terreratman@users.noreply.github.com>
2023-12-29 18:22:42 -0800

    Used built-in timer instead

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -1,17 +1,14 @@
 extends Area3D
 
+class_name Bullet
+
 var speed : float = 30.0
 var damage : int = 1
-var lifetime : float = 1.0
 
 # called every frame
 func _process(delta):
 	# move the bullet forwards
 	position += global_transform.basis.z * speed * delta
-	lifetime -= delta
-	if lifetime <= 0:
-		print("destroying bullet now")
-		destroy()
 
 # destroys the bullet
 func destroy ():
