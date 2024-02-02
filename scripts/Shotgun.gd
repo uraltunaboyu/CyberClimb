04b70b72da504c7f837691c91b2bed775a940933
terreratman <53959686+terreratman@users.noreply.github.com>
2024-02-01 21:47:06 -0800

    Implemented shotgun spread and bullet generation, and general gun mechanics

--- scripts/Shotgun.gd
+++ scripts/Shotgun.gd
@@ -2,18 +2,34 @@ extends Node3D
 
 @onready var muzzle : Node3D = get_node("Muzzle")
 @onready var bulletScene = preload("res://scenes/Bullet.tscn")
+
 var ammoCount = 100
+var bullet_speed = 5
+var bullet_damage = 1
+var pellet_count = 10
+var spread_angle:float = 0.25 # Cone with this angle (radians)
+var bullet_life = 1
 
 func _ready():
-	print("Sun's out guns out")
+	return
 	
 func attack():
 	if ammoCount == 0: return
 	
-	var bullet = bulletScene.instantiate()
-	get_node("/root/MainScene").add_child(bullet)
-	
-	bullet.global_transform = muzzle.global_transform
+	for i in range(pellet_count):
+		var bullet = bulletScene.instantiate()
+		bullet.initialize(bullet_speed, bullet_damage, bullet_life)
+		
+		get_node("/root/MainScene").add_child(bullet)
+		bullet.global_transform = muzzle.global_transform
+		
+		var angle_x = randf_range(spread_angle, -spread_angle)
+		var angle_y = randf_range(spread_angle, -spread_angle)
+		
+		bullet.rotate_object_local(Vector3(1,0,0), angle_x)
+		bullet.rotate_object_local(Vector3(0,1,0), angle_y)
+		
+		
 	
 	ammoCount -= 1
 	
