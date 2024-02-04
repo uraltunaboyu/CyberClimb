dfcf6268b18aa4da1d0904e26cde089233aa3a83
terreratman <53959686+terreratman@users.noreply.github.com>
2024-02-04 01:09:20 -0800

    Added some comments + differentiation between semi and full auto guns
    Comments added for DamageVis and Enemy scripts.
    "shoot" functionality fully remmoved from Player script and moved to GunSlot.
    Fully automatic weapons created with an easily variable fire-rate.
    "shoot" function now differentiates between semi and full auto weapons,
    with classes made for each (currently on the AssultRifle and Shotgun scripts.

--- scripts/GunSlot.gd
+++ scripts/GunSlot.gd
@@ -1,15 +1,24 @@
 extends Node
 
-@export var target_scene = load("res://scenes/Shotgun.tscn")
+@export var target_scene = load("res://scenes/AssultRifle.tscn")
+@onready var ui : Node = get_node("/root/MainScene/CanvasLayer/UI")
 var equipped_gun
 
 func _ready():
 	equipped_gun = target_scene.instantiate()
 	add_child(equipped_gun)
 	equipped_gun.global_transform = self.global_transform
-
+	
+func _process(delta):
+	if Input.is_action_pressed("shoot") and equipped_gun is auto_gun:
+		attack()
+	if Input.is_action_just_pressed("shoot"):
+		attack()
+		
 func attack():
-	equipped_gun.attack()
+	if equipped_gun:
+		equipped_gun.attack()
+	ui.update_ammo_text(equipped_gun.get_ammo_count())
 
 func get_ammo_count():
 	return equipped_gun.get_ammo_count()
