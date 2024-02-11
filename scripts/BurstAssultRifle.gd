6b692ee91e579c332a409eea22ee9e6837e30091
terreratman <53959686+terreratman@users.noreply.github.com>
2024-02-10 19:07:54 -0800

    Added a default Gun class, cleaned up some scripts.
    Default gun class contains all common functions and a shared
    bullet generation function that the attack() functions of
    individual guns can call.
    
    All weapons now extend Gun, some extend sub-classes based on Gun
    
    Some clean up adding a couple comments and deleting unwanted debug
    print statements from earlier.

--- scripts/BurstAssultRifle.gd
+++ scripts/BurstAssultRifle.gd
@@ -1,55 +1,33 @@
-class_name BurstGun extends Node3D
+class_name BurstGun extends Gun
 
-@onready var muzzle : Node3D = $Muzzle
-@onready var bulletScene = preload("res://scenes/Bullet.tscn")
-@onready var shot_timer : Timer = $Timer
 @onready var burst_timer : Timer = $BurstTimer
 
-var ammoCount = 50
-var bullet_speed = 18
-var bullet_damage_min = 15
-var bullet_damage_max = 20
-var spread_angle:float = 0.03 # Cone with this angle (radians)
-var bullet_life = 1
-var rpm:float = 80
 var burst_shots = 3
 var burst_delay = 0.05
-var burst_left = burst_shots
 
 func _ready():
-	shot_timer.wait_time = 60/rpm
+	ammoCount = 30
+	bullet_speed = 20
+	bullet_damage_min = 20
+	bullet_damage_max = 25
+	spread_angle = 0.06
+	bullet_life = 2
+	shots_per_sec = 1.33
+	shot_timer.wait_time = 1/shots_per_sec
 	shot_timer.one_shot = true
 	burst_timer.wait_time = burst_delay
 	burst_timer.one_shot = true
-	return
 	
 func multiple_attacks():
 	for i in burst_shots:
 		if ammoCount != 0:
 			burst_timer.start()
 			await burst_timer.timeout
-			var bullet = bulletScene.instantiate()
-			bullet.initialize(bullet_speed, randi_range(bullet_damage_min, bullet_damage_max), bullet_life)
 			
-			get_node("/root/MainScene").add_child(bullet)
-			bullet.global_transform = muzzle.global_transform
-			
-			var angle_x = randfn(0, spread_angle/1.5)
-			var angle_y = randfn(0, spread_angle/1.5)
-			
-			bullet.rotate_object_local(Vector3(1,0,0), angle_x)
-			bullet.rotate_object_local(Vector3(0,1,0), angle_y)
-			
+			make_bullet()
 			ammoCount -= 1
 
 func attack():
 	if ammoCount!= 0 and shot_timer.is_stopped():
 		multiple_attacks()
 		shot_timer.start()
-		
-
-func get_ammo_count():
-	return ammoCount
-
-func add_ammo_count(count):
-	ammoCount += count
--- scripts/Pistol.gd
+++ scripts/Pistol.gd
@@ -1,13 +1,13 @@
-class_name AutoGun extends Gun
+class_name SemiAutoGun extends Gun
 
 func _ready():
-	ammoCount = 40
+	ammoCount = 100
 	bullet_speed = 15
-	bullet_damage_min = 11
-	bullet_damage_max = 15
-	spread_angle = 0.06
-	bullet_life = 2
-	shots_per_sec = 20
+	bullet_damage_min = 8
+	bullet_damage_max = 12
+	spread_angle = 0.05 # Cone with this angle (radians)
+	bullet_life = 1.3
+	shots_per_sec = 3
 	shot_timer.wait_time = 1/shots_per_sec
 	shot_timer.one_shot = true
 	
