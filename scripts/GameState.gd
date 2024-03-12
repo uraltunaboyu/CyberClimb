24317ccdf9abdb566ff9ae1bdc32a58b7c496fac
Ural Tunaboyu <ural2000@gmail.com>
2024-03-11 17:01:29 -0700

    Added gamestate state machine, can pause game

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -8,6 +8,22 @@ enum ProgressionFlag {
 	FIRST_BOSS
 }
 
+enum GameStates {
+	MAIN_MENU,
+	HUB,
+	PLAYING,
+	SHOP,
+	PAUSED
+}
+
+var current_state: GameStates = GameStates.MAIN_MENU:
+	set(new_state):
+		if (new_state == GameStates.PLAYING || new_state == GameStates.HUB):
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		current_state = new_state
+
 var completed_flags = [ProgressionFlag.NONE]
 
 # TODO figure out inventory
@@ -20,6 +36,24 @@ var player_stats = {}
 var player_vitals = {}
 
 var level_controller = LevelController.new()
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+func _process(_delta):
+	if Input.is_action_just_pressed("exit"):
+		if current_state == GameStates.MAIN_MENU:
+			get_tree().quit()
+		elif current_state != GameStates.PAUSED:
+			current_state = GameStates.PAUSED
+			get_tree().paused = true
+		else:
+			current_state = GameStates.PLAYING;
+			get_tree().paused = false
+
+func set_state_playing():
+	current_state = GameStates.PLAYING
+	
 
 func is_flag_complete(flag: ProgressionFlag) -> bool:
 	return completed_flags.has(flag)
