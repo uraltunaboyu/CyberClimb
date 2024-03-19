0fc5d025d86aa891a86cec6a4b5956d214921db3
Ural Tunaboyu <ural2000@gmail.com>
2024-03-18 20:46:59 -0700

    Start of enemy spawn logic

--- scripts/SpawnArea.gd
+++ scripts/SpawnArea.gd
@@ -1,12 +1,17 @@
 extends Area3D
 
+const EnemyAttributes = preload("res://scripts/EnemyAttributes.gd")
+
 @export var min_enemies : int = 0
 @export var max_enemies : int
+@export var acceptable_enemies : Array[EnemyAttributes.EnemyTag]
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func spawn_appropriate_enemy(diff: int):
+	var all_enemies = []
+	for tag in acceptable_enemies:
+		all_enemies.append_array(EnemyAttributes.ENEMY_BY_TAG[tag])
+		
+	var possible_enemies = EnemyAttributes.intersect(all_enemies, EnemyAttributes.ENEMY_BY_DIFFICULTY[diff])
+	# TODO: choose random enemy out of array
+	# TODO: spawn enemy in level
+	# TODO: maybe return enemy cost?
