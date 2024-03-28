d279e101f4d14c66f087b57a6746fdeaa9903c6d
Kagan Kuscu <kgnksc@gmail.comn>
2024-03-27 19:52:15 -0700

    Added Main Menu and Options Menu layout

---
+++ scripts/OptionMenu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_mute_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
