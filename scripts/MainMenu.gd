20f9771826d8ea2bcbf95331e68bda3162ae469a
Kagan Kuscu <kgnksc@gmail.comn>
2024-03-30 16:25:02 -0700

    Added Mute button and options popup

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,19 +1,27 @@
 extends Control
 
 
+
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	#$MarginContainer/VBoxContainer/StartButton.grab_focus()
+func _ready():
+	AudioController.play_hub_music()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+@onready var OptionMenu = $OptionMenu
+@onready var MuteButton = $OptionMenu/MuteButton
+@onready var BackButton = $OptionMenu/BackButton
+var music_bus = AudioServer
+
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/MainScene.tscn")
 
 func _on_option_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/OptionMenu.tscn")
+	OptionMenu.z_index = 1
+	MuteButton.disabled = false
+	BackButton.disabled = false
 
 func _on_quit_button_pressed():
 	get_tree().quit()
@@ -21,4 +29,10 @@ func _on_quit_button_pressed():
 func _on_credits_button_pressed():
 	pass # Replace with function body.
 
+func _on_back_button_pressed():
+	OptionMenu.z_index = -1
+	MuteButton.disabled = true
+	BackButton.disabled = true
 
+func _on_mute_button_pressed():
+	AudioController.toggle_mute()
